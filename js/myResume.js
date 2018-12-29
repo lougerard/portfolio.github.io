f57d8f168a02e429a7fc4f68d9190bc218bc4cf4
lougerard <louis.gerard@student.uclouvain.be>
2018-12-29 17:10:47 +0100

    add new portfolio

---
+++ js/myResume.js
@@ -0,0 +1,426 @@
+$(document).ready(function() {
+    $('#cv').click(function(){
+        $("#dialog").dialog({
+            modal: true,
+            width: 850,
+            height: 650,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_1').click(function(){
+        $("#certif_1").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+
+    $('#proof_2').click(function(){
+        $("#certif_2").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_3').click(function(){
+        $("#certif_3").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_4').click(function(){
+        $("#certif_4").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_5').click(function(){
+        $("#certif_5").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_6').click(function(){
+        $("#certif_6").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_7').click(function(){
+        $("#certif_7").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_8').click(function(){
+        $("#certif_8").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_9').click(function(){
+        $("#certif_9").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_10').click(function(){
+        $("#certif_10").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_11').click(function(){
+        $("#certif_11").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_12').click(function(){
+        $("#certif_12").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_13').click(function(){
+        $("#certif_13").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_14').click(function(){
+        $("#certif_14").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_15').click(function(){
+        $("#certif_15").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_16').click(function(){
+        $("#certif_16").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_17').click(function(){
+        $("#certif_17").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_18').click(function(){
+        $("#certif_18").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+    $('#proof_19').click(function(){
+        $("#certif_19").dialog({
+            modal: true,
+            width: 1000,
+            height: 800,
+            resizable: false,
+            dragable: false,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                    }
+
+                }
+            ]
+        });
+    });
+
+    $('#myTable').DataTable({
+        columnDefs: [
+            {
+                targets: -1,
+                className: 'dt-body-right'
+            }
+        ],
+        "footerCallback": function ( row, data, start, end, display ) {
+            var api = this.api(), data;
+
+            // Remove the formatting to get integer data for summation
+            var intVal = function ( i ) {
+                return typeof i === 'string' ?
+                    i.replace(/[\$,]/g, '')*1 :
+                    typeof i === 'number' ?
+                        i : 0;
+            };
+
+            // Total over all pages col worked hours
+            total2 = api
+                .column( 2 )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+            // Total over all pages col valued hours
+            total = api
+                .column( 3 )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Total over this page col worked hours
+            pageTotal2 = api
+                .column( 2, { page: 'current'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Total over this page col valued hours
+            pageTotal = api
+                .column( 3, { page: 'current'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Update footer
+            $( api.column( 3 ).footer() ).html(
+                pageTotal +' h on '+ total +' h'
+            );
+            $( api.column( 2 ).footer() ).html(
+                pageTotal2 +' h on '+ total2 +' h'
+            );
+        }
+
+    });
+
+
+});
+
